b232597891a59b38a42442da064355243310a54f
Aryashree Pritikrishna <aryashreep@gmail.com>
2020-12-03 18:09:03 +0530

    Added panchatatva mantra.

--- assets/js/japa_mala.js
+++ assets/js/japa_mala.js
@@ -11,9 +11,9 @@ function startJapa(){
         iRounds = isNaN(iRounds) ? 0 : iRounds;
         iBeads = 0;
         iRounds++;
-        document.getElementById("beads").style.display = "none";
+        document.getElementById("beads").style.display = "hidden";
         document.getElementById("rounds").innerHTML = iRounds;
-        document.getElementById("beadsButton").innerHTML = '<img src="./assets/images/panchatatva_mantra.png" width="180" height="180" id="panchatatva" alt="Panchatatva Mantra" />';
+        document.getElementById("beadsButton").innerHTML = '<span id="beads" style="display: none;"></span><img src="./assets/images/panchatatva_mantra.png" width="180" height="180" id="panchatatva" alt="Panchatatva Mantra" />';
         if (navigator.vibrate) {
             navigator.vibrate(1000);
         }
@@ -22,6 +22,9 @@ function startJapa(){
     {
         iBeads = isNaN(iBeads) ? 0 : iBeads;
         iBeads++;
+        if (navigator.vibrate) {
+            navigator.vibrate(100);
+        }
         document.getElementById("beads").style.display = "block";
         document.getElementById("beads").innerHTML = iBeads;
         document.getElementById("panchatatva").style.display = "none";
